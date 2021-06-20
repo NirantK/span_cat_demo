919eef35d0544ada631272b6a2027a2955a4e93d
Nirant <NirantK@users.noreply.github.com>
2021-06-20 17:27:27 +0530

    WIP commit with everything in convert.py

--- scripts/convert.py
+++ scripts/convert.py
@@ -1,35 +1,82 @@
-"""Convert entity annotation from spaCy v2 TRAIN_DATA format to spaCy v3
-.spacy format."""
-import srsly
+"""Convert entity annotation from HuggingFace datasets IOB format to spaCy v3 .spacy format."""
 import typer
-import warnings
-from pathlib import Path
+from datasets import load_dataset
 
 import spacy
-from spacy.tokens import DocBin
+from pathlib import Path
+from spacy.tokens import DocBin, Span
+from typing import List
 
 
-def convert(lang: str, input_path: Path, output_path: Path):
+def download():
+    dataset = load_dataset("conll2003")
+    return dataset
+
+
+def retokenize(tokens: List) -> str:
+    return " ".join(tokens)
+
+
+def convert(
+    lang: str, output_path: Path, split: str, spans_key: str = "sc", count: int = None
+):
     nlp = spacy.blank(lang)
+    dataset = load_dataset("conll2003")
+    splits = list(dataset.keys())
+    assert split in splits
+    data = dataset[split]
+    tags_key = data.features["ner_tags"].feature.names
     db = DocBin()
-    for text, annot in srsly.read_json(input_path):
-        doc = nlp.make_doc(text)
-        ents = []
-        for start, end, label in annot["entities"]:
-            span = doc.char_span(start, end, label=label)
-            if span is None:
-                msg = f"Skipping entity [{start}, {end}, {label}] in the following text because the character span '{doc.text[start:end]}' does not align with token boundaries:\n\n{repr(text)}\n"
-                warnings.warn(msg)
-            else:
-                ents.append(span)
-        try:
-            doc.ents = ents
-        except:
-            msg = f"Skipping sentence {text}, {annot}"
-            # warnings.warn(msg)
+    docs = []
+    for i, element in enumerate(data):
+        if count is not None and i > count:
+            break
+        sentence = retokenize(element["tokens"])
+        doc = nlp(sentence)
+        ner_tags = [tags_key[k] for k in element["ner_tags"]]
+        char_spans, char_len = [], 0
+        substring = ""
+        j, prev_begin = 0, False
+        for j, tag in enumerate(ner_tags):
+            token = element["tokens"][j]
+            if "B-" in tag:
+                if prev_begin:
+                    char_spans.append({"start": start, "end": char_len, "label": label})
+                    substring = ""
+                substring += token
+                label = tag[2:]
+                start = char_len
+                prev_begin = True
+            if "I-" in tag:
+                substring += " "
+                substring += token
+                prev_begin = False
+            if "O" == tag and len(substring) > 0:
+                char_spans.append({"start": start, "end": char_len, "label": label})
+                substring = ""
+                prev_begin = False
+            char_len += len(token)
+            char_len += 1  # for the space
+
+        if len(substring) > 0:  # jugaad for when the last tag is "I-"
+            char_spans.append(
+                {
+                    "start": start,
+                    "end": char_len,
+                    "label": label,
+                }
+            )
+        for span in char_spans:
+            start, end, label = span["start"], span["end"], span["label"]
+            new_ent = doc.char_span(start, end - 1, label=label)
+            doc.set_ents([new_ent], default="unmodified")
+
+        doc.spans[spans_key] = list(doc.ents)
+        docs.append(doc)
         db.add(doc)
     db.to_disk(output_path)
 
 
 if __name__ == "__main__":
+    typer.run(download)
     typer.run(convert)
--- scripts/download.py
+++ scripts/download.py
@@ -1,9 +1,6 @@
 import fire
 from datasets import load_dataset
 
-def download() -> None:
-    dataset = load_dataset("conll2003")
-    # return dataset
 
 if __name__ == "__main__":
     fire.Fire(download)
