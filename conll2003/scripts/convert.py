f8e609824d75c62e6db2975c1cf32db13b47a70d
Nirant <NirantK@users.noreply.github.com>
2021-06-29 12:47:21 +0530

    Hack convert #2

--- conll2003/scripts/convert.py
+++ conll2003/scripts/convert.py
@@ -11,7 +11,7 @@ def main(loc: Path, lang: str, spans_key: str):
     overlapping or nested annotations, which can't be represented on the
     per-token level.
     """
-    nlp = spacy.blank(lang)
+    nlp = spacy.blank("en_core_web_sm")
     docbin = DocBin().from_disk(loc)
     docs = list(docbin.get_docs(nlp.vocab))
     for doc in docs:
