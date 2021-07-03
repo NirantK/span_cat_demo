cf2a39c7722483246362b293e2a05518332826cc
Nirant <NirantK@users.noreply.github.com>
2021-07-03 14:27:43 +0530

    Add utility functions

--- conll2003/functions.py
+++ conll2003/functions.py
@@ -3,11 +3,68 @@ import spacy
 from spacy import registry
 from spacy.tokens import Doc, DocBin, Span
 from spacy.matcher import PhraseMatcher
-from typing import List, Callable, Optional
+from typing import List, Callable, Optional, Any
 from thinc.types import Ragged
 from thinc.api import Config, Model, get_current_ops, set_dropout_rate, Ops, to_numpy
 from pathlib import Path
 import numpy as np
+
+def from_indices(indices: List[Any], lengths:List, *, ops: Optional[Ops] = None):
+    """
+    Make Ragged spans for training and inference. 
+    Used as a data type convertor in suggester for Span Categorizer
+
+    Args:
+        indices (List[Any]): Python list with np, ops.xp, or List elements
+        lengths (List): Number of span elements in each doc
+
+    Raises:
+        ValueError: In case of input values not meeting expected data ranges
+        TypeError: Invalid datatype as input
+
+    Returns:
+        Ragged: the Thinc datatype for training
+    """
+    if ops is None:
+        ops = get_current_ops()
+
+    # check if sum of lengths is same as length of indices, if not raise ValueError
+    if not np.allclose(np.sum(lengths), len(indices)):
+        raise ValueError("Sum of lengths of indices and lengths of spans do not match.")
+    sum_of_lengths = sum(lengths)
+    number_of_indices = len(indices)
+    
+    if number_of_indices <= 0:
+        raise ValueError(f"There were no (start, end) pairs found. Check if indices input is empty")
+    
+    # check if any element is None, if yes raise ValueError
+    if any(x is None for x in indices):
+        raise ValueError(f"There were (start, end) pairs with None values. Check if indices input is empty")
+    
+    if not sum_of_lengths == number_of_indices:
+        raise ValueError(f"The sum of all values in lengths is {sum_of_lengths}, while there are only {number_of_indices} (start, end) pairs. They're expected to be equal")
+    if type(indices) == type([]) and type(indices[-1]) == type(ops.xp.array([1])):
+        indices = ops.xp.array(indices) 
+        if indices.ndim != 2:
+            raise ValueError(f"Expected indices to be a 2d matrix, with each row being a start, end pair")
+
+    output = Ragged(indices, ops.asarray(lengths, dtype="i"))
+    assert output.dataXd.ndim == 2
+
+    return output
+
+def from_spans(spans: List, docs: List[Doc], *, ops: Optional[Ops] = None)->Ragged:
+    """
+    Convert a list of spans into a Ragged object
+    """
+    if ops is None:
+        ops = get_current_ops()
+    indices = []
+    lengths = []
+    for doc, span in zip(docs, spans):
+        indices.append(ops.xp.array([span.start, span.end]))
+        lengths.append(span.end - span.start)
+    return from_indices(indices, lengths, ops=ops)
 
 
 @registry.misc("nounchunk_ngram_suggester.v1")
@@ -20,8 +77,6 @@ def nounchunk_ngram_suggester(sizes: List[int], train_corpus: Path) -> Callable[
 
     def ngram_suggester(docs: List[Doc], *, ops: Optional[Ops] = None) -> Ragged:
 
-        if ops is None:
-            ops = get_current_ops()
         spans = []
         lengths = []
 
@@ -29,15 +84,13 @@ def nounchunk_ngram_suggester(sizes: List[int], train_corpus: Path) -> Callable[
             doc_spans = []
             starts = ops.xp.arange(len(doc), dtype="i")
             starts = starts.reshape((-1, 1))
-            length, noun_length = 0, 0
+            length = 0
             for size in sizes:
                 if size <= len(doc):
                     starts_size = starts[: len(doc) - (size - 1)]
                     ngrams = ops.xp.hstack((starts_size, starts_size + size))
                     doc_spans.extend([element for element in ngrams])
                     length += len(ngrams)
-                # if spans:
-                #     assert spans[-1].ndim == 2, spans[-1].shape
 
             new_doc = nlp(doc.text)       
             for chunk in new_doc.noun_chunks:
@@ -58,8 +111,6 @@ def nounchunk_ngram_suggester(sizes: List[int], train_corpus: Path) -> Callable[
         
         spans = list(set(spans))
         if len(spans) > 0:
-            # element = ops.xp.vstack(spans)
-            # print(type(spans), len(spans), len(lengths))
             spans = ops.xp.array(spans)
             assert len(spans) == sum(lengths)
             # print(type(spans), len(spans), len(lengths))
