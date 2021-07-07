dd7cee9f5bb85ac9a60d80169fe9e353c10abbb0
Nirant <NirantK@users.noreply.github.com>
2021-07-07 15:05:46 +0530

    Refmt for readability

--- conll2003/functions.py
+++ conll2003/functions.py
@@ -1,17 +1,22 @@
-from matplotlib.pyplot import axis
-import spacy
-from spacy import registry
-from spacy.tokens import Doc, DocBin, Span
-from spacy.matcher import PhraseMatcher
-from typing import List, Callable, Optional, Any
-from thinc.types import Ragged
-from thinc.api import Config, Model, get_current_ops, set_dropout_rate, Ops, to_numpy
-from pathlib import Path
-import numpy as np
+import random
 
-def from_indices(indices: List[Any], lengths:List, ops: Optional[Ops] = None)->Ragged:
+import numpy as np
+import spacy
+from matplotlib.pyplot import axis
+from spacy import registry
+from spacy.matcher import PhraseMatcher
+from spacy.tokens import Doc, DocBin, Span
+from thinc.api import (Config, Model, Ops, get_current_ops, set_dropout_rate,
+                       to_numpy)
+from thinc.types import Ragged
+
+from pathlib import Path
+from typing import Any, Callable, List, Optional
+
+
+def from_indices(indices: List[Any], lengths: List, ops: Optional[Ops] = None) -> Ragged:
     """
-    Make Ragged spans for training and inference. 
+    Make Ragged spans for training and inference.
     Used as a data type convertor in suggester for Span Categorizer
 
     Args:
@@ -28,24 +33,33 @@ def from_indices(indices: List[Any], lengths:List, ops: Optional[Ops] = None)->R
     # check if sum of lengths is same as length of indices, if not raise ValueError
     if not np.allclose(np.sum(lengths), len(indices)):
         raise ValueError("Sum of lengths of indices and lengths do not match.")
-    
+
     if len(indices) == 0:
-        raise ValueError(f"There were no (start, end) pairs found. Check if indices input is empty")
-    
+        raise ValueError(
+            f"There were no (start, end) pairs found. Check if indices input is empty"
+        )
+
     # check if any element is None, if yes raise ValueError
     if any(x is None for x in indices):
-        raise ValueError(f"There were (start, end) pairs with None values. Check if indices input is correct")
-    
+        raise ValueError(
+            f"There were (start, end) pairs with None values. Check if indices input is correct"
+        )
+
     if type(indices) == type([]) and type(indices[-1]) == type(ops.xp.array([1])):
-        indices = ops.xp.array(indices) 
+        indices = ops.xp.array(indices)
         if indices.ndim != 2:
-            raise ValueError(f"Expected indices to be a 2d matrix, with each row being a [start, end] pair")
+            raise ValueError(
+                f"Expected indices to be a 2d matrix, with each row being a [start, end] pair"
+            )
 
     output = Ragged(indices, ops.asarray(lengths, dtype="i"))
     assert output.dataXd.ndim == 2
     return output
 
-def from_spans(span_groups: List[List], docs: List[Doc], ops: Optional[Ops] = None)->Ragged:
+
+def from_spans(
+    span_groups: List[List], docs: List[Doc], ops: Optional[Ops] = None
+) -> Ragged:
     """
     Convert a list of spans into a Ragged object
     """
@@ -55,16 +69,19 @@ def from_spans(span_groups: List[List], docs: List[Doc], ops: Optional[Ops] = No
     lengths = []
     for doc, spans in zip(docs, span_groups):
         for span in spans:
-            start, end  = span.start, span.end
-            assert start < end 
+            start, end = span.start, span.end
+            assert start < end
             assert end <= len(doc)
             indices.append(ops.xp.array([start, end]))
-            
+
         lengths.append(len(spans))
     return from_indices(indices, lengths, ops=ops)
 
+
 @registry.misc("entity_suggester.v1")
-def build_entity_suggester(model:str ="en_core_web_sm", make_diff_doc: bool = True) -> Callable[[List[Doc], List[str]], Ragged]:
+def build_entity_suggester(
+    model: str = "en_core_web_sm", make_diff_doc: bool = True
+) -> Callable[[List[Doc], List[str]], Ragged]:
     """
     Suggester which uses the spaCy Entity Recognizer to suggest spans.
     """
@@ -81,7 +98,7 @@ def build_entity_suggester(model:str ="en_core_web_sm", make_diff_doc: bool = Tr
 
         for doc in docs:
             doc_spans = []
-            new_doc = nlp(doc.text)       
+            new_doc = nlp(doc.text)
             for ent in new_doc.ents:
                 char_start, char_end = ent.start_char, ent.end_char
                 span = doc.char_span(char_start, char_end)
@@ -89,20 +106,19 @@ def build_entity_suggester(model:str ="en_core_web_sm", make_diff_doc: bool = Tr
                     doc_spans.append(span)
 
             span_groups.append(doc_spans)
-    
+
         return from_spans(span_groups, docs, ops)
 
     return entity_suggester
 
-import random
 
 @registry.misc("random_suggester.v1")
-def build_random_spam_suggester(sizes: List)->Callable:
+def build_random_suggester(sizes: List) -> Callable:
     """
     Suggests random spans for each doc.
 
     Args:
-        sizes (List): 
+        sizes (List):
 
     Returns:
         Callable:
@@ -117,17 +133,17 @@ def build_random_spam_suggester(sizes: List)->Callable:
             ops = get_current_ops()
 
         spans, lengths = [], []
-        for doc in docs: 
+        for doc in docs:
             length = 0
             token_count = len(doc)
             doc_spans = []
-            while len(doc_spans) < 2 :
-                start = random.choice(range(token_count-2))
+            while len(doc_spans) < 2:
+                start = random.choice(range(token_count - 2))
                 end = start + 1
                 element = ops.xp.array([start, end])
                 doc_spans.append(element)
                 length += 1
-            
+
             if len(doc_spans) > 0 and len(doc_spans) == length:
                 spans.extend(doc_spans)
                 lengths.append(length)
@@ -141,17 +157,22 @@ def build_random_spam_suggester(sizes: List)->Callable:
 
     return random_suggester
 
+
 @registry.misc("nounchunk_ngram_suggester.v1")
-def build_nounchunk_ngram_suggester(sizes: List[int], train_corpus: Path) -> Callable[[List[Doc]], Ragged]:
+def build_nounchunk_ngram_suggester(
+    sizes: List[int], train_corpus: Path
+) -> Callable[[List[Doc]], Ragged]:
     """Suggest all spans of the given lengths. Spans are returned as a ragged
     array of integers. The array has two columns, indicating the start and end
     position."""
-    
+
     nlp = spacy.load("en_core_web_sm")
 
-    def nounchunk_ngram_suggester(docs: List[Doc], *, ops: Optional[Ops] = None) -> Ragged:
+    def nounchunk_ngram_suggester(
+        docs: List[Doc], *, ops: Optional[Ops] = None
+    ) -> Ragged:
         if ops is None:
-            ops = get_current_ops()  
+            ops = get_current_ops()
 
         spans = []
         lengths = []
@@ -168,7 +189,7 @@ def build_nounchunk_ngram_suggester(sizes: List[int], train_corpus: Path) -> Cal
                     doc_spans.extend([element for element in ngrams])
                     length += len(ngrams)
 
-            new_doc = nlp(doc.text)       
+            new_doc = nlp(doc.text)
             for chunk in new_doc.noun_chunks:
                 char_start, char_end = chunk.start_char, chunk.end_char
                 span = doc.char_span(char_start, char_end)
@@ -178,13 +199,13 @@ def build_nounchunk_ngram_suggester(sizes: List[int], train_corpus: Path) -> Cal
                     assert len(element) == 2
                     doc_spans.append(element)
                     length += 1
-            
+
             assert length == len(doc_spans)
             lengths.append(length)
             if length == 0:
                 raise ValueError("Length is 0")
             spans.extend(ops.xp.array(doc_spans))
-        
+
         # spans = list(set(spans))
         if len(spans) > 0:
             spans = ops.xp.array(spans)
@@ -201,25 +222,25 @@ def build_nounchunk_ngram_suggester(sizes: List[int], train_corpus: Path) -> Cal
     return nounchunk_ngram_suggester
 
 
-
 @registry.misc("train_ngram_suggester.v1")
-def train_ngram_suggester(sizes: List[int], train_corpus: Path) -> Callable[[List[Doc]], Ragged]:
+def train_ngram_suggester(
+    sizes: List[int], train_corpus: Path
+) -> Callable[[List[Doc]], Ragged]:
     """Suggest all spans of the given lengths. Spans are returned as a ragged
     array of integers. The array has two columns, indicating the start and end
     position."""
-    
+
     # Prepare matcher
-    nlp = spacy.blank("en") 
+    nlp = spacy.blank("en")
     docbin = DocBin().from_disk(train_corpus)
     train_docs = list(docbin.get_docs(nlp.vocab))
     patterns = set()
     for doc in train_docs:
         for ent in doc.ents:
             patterns.add(nlp.make_doc(ent.text))
-            
+
     matcher = PhraseMatcher(nlp.vocab)
     matcher.add("ENT", list(patterns))
-
 
     def ngram_suggester(docs: List[Doc], *, ops: Optional[Ops] = None) -> Ragged:
         if ops is None:
@@ -238,17 +259,16 @@ def train_ngram_suggester(sizes: List[int], train_corpus: Path) -> Callable[[Lis
                     spans.extend([element for element in ngrams])
                     length += len(ngrams)
                 # if spans:
-                    # assert spans[-1].ndim == 2, spans[-1].shape
-            
+                # assert spans[-1].ndim == 2, spans[-1].shape
 
             matches = matcher(doc, as_spans=True)
             for span in matches:
                 element = ops.xp.hstack((span.start, span.end))
                 spans.append(element)
-                length+=1
-            
+                length += 1
+
             lengths.append(length)
-        
+
         if len(spans) > 0:
             spans = ops.xp.asarray(spans)
             output = Ragged(ops.xp.vstack(spans), ops.asarray(lengths, dtype="i"))
---
+++ conll2003/test_functions.py
@@ -0,0 +1,123 @@
+import numpy as np
+import spacy
+from matplotlib.pyplot import axis
+from numpy.testing import assert_equal
+from spacy import registry
+from spacy.language import Language
+from spacy.matcher import PhraseMatcher
+from spacy.tokens import Doc, DocBin, Span
+from spacy.training import Example
+from spacy.util import fix_random_seed, registry
+from thinc.api import (Config, Model, Ops, get_current_ops, set_dropout_rate,
+                       to_numpy)
+from thinc.types import Ragged
+
+import pytest
+from pathlib import Path
+from typing import Any, Callable, List, Optional
+
+from .functions import *
+
+SPAN_KEY = "sc"
+
+
+def test_ngram_suggester(en_tokenizer):
+    # test different n-gram lengths
+    for size in [1, 2, 3]:
+        ngram_suggester = registry.misc.get("spacy.ngram_suggester.v1")(sizes=[size])
+        docs = [
+            en_tokenizer(text)
+            for text in [
+                "a",
+                "a b",
+                "a b c",
+                "a b c d",
+                "a b c d e",
+                "a " * 100,
+            ]
+        ]
+        ngrams = ngram_suggester(docs)
+        # span sizes are correct
+        for s in ngrams.data:
+            assert s[1] - s[0] == size
+        # spans are within docs
+        offset = 0
+        for i, doc in enumerate(docs):
+            spans = ngrams.dataXd[offset : offset + ngrams.lengths[i]]
+            spans_set = set()
+            for span in spans:
+                assert 0 <= span[0] < len(doc)
+                assert 0 < span[1] <= len(doc)
+                spans_set.add((span[0], span[1]))
+            # spans are unique
+            assert spans.shape[0] == len(spans_set)
+            offset += ngrams.lengths[i]
+        # the number of spans is correct
+        assert_equal(ngrams.lengths, [max(0, len(doc) - (size - 1)) for doc in docs])
+
+    # test 1-3-gram suggestions
+    ngram_suggester = registry.misc.get("spacy.ngram_suggester.v1")(sizes=[1, 2, 3])
+    docs = [en_tokenizer(text) for text in ["a", "a b", "a b c", "a b c d", "a b c d e"]]
+    ngrams = ngram_suggester(docs)
+    assert_equal(ngrams.lengths, [1, 3, 6, 9, 12])
+    assert_equal(
+        ngrams.data,
+        [
+            # doc 0
+            [0, 1],
+            # doc 1
+            [0, 1],
+            [1, 2],
+            [0, 2],
+            # doc 2
+            [0, 1],
+            [1, 2],
+            [2, 3],
+            [0, 2],
+            [1, 3],
+            [0, 3],
+            # doc 3
+            [0, 1],
+            [1, 2],
+            [2, 3],
+            [3, 4],
+            [0, 2],
+            [1, 3],
+            [2, 4],
+            [0, 3],
+            [1, 4],
+            # doc 4
+            [0, 1],
+            [1, 2],
+            [2, 3],
+            [3, 4],
+            [4, 5],
+            [0, 2],
+            [1, 3],
+            [2, 4],
+            [3, 5],
+            [0, 3],
+            [1, 4],
+            [2, 5],
+        ],
+    )
+
+    # test some empty docs
+    ngram_suggester = registry.misc.get("spacy.ngram_suggester.v1")(sizes=[1])
+    docs = [en_tokenizer(text) for text in ["", "a", ""]]
+    ngrams = ngram_suggester(docs)
+    assert_equal(ngrams.lengths, [len(doc) for doc in docs])
+
+    # test all empty docs
+    ngram_suggester = registry.misc.get("spacy.ngram_suggester.v1")(sizes=[1])
+    docs = [en_tokenizer(text) for text in ["", "", ""]]
+    ngrams = ngram_suggester(docs)
+    assert_equal(ngrams.lengths, [len(doc) for doc in docs])
+
+
+def test_spans_ops_missing():
+    """Should raise a Value Error when ops is None"""
+    sentence = ""
+
+    with pytest.raises(ValueError):
+        from_spans()
